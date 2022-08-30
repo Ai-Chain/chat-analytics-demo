3591914212d21a6e491662dcf69a3d61f2f9bd71
Enias Cailliau <hello@enias.io>
2022-08-30 16:05:14 +0200

    Add new login constraints

---
+++ src/auth.py
@@ -0,0 +1,131 @@
+import asyncio
+import base64
+import random
+import time
+from pathlib import Path
+from typing import Union, Dict
+
+import gspread
+import streamlit as st
+from httpx_oauth.clients.google import GoogleOAuth2
+from httpx_oauth.oauth2 import GetAccessTokenError
+
+USAGE_LIMIT = 10
+APP_ID = "chat"
+
+
+@st.cache(ttl=3600, allow_output_mutation=True)
+def get_worksheet():
+    gc = gspread.service_account(filename='src/gcred.json')
+    sh = gc.open_by_key('1Dnf4gqdicSXwL4-2laeIYGR0bOeLU6tI_GtxGme4m7s')
+    worksheet = sh.worksheet("Contacts")
+    return worksheet
+
+
+def check_usage(usage_stats: Dict[str, Union[str, int]]):
+    placeholder = st.session_state["placeholder"]
+    if usage_stats:
+
+        if usage_stats[APP_ID] > USAGE_LIMIT:
+            placeholder.markdown(
+                f"Signed in as {usage_stats['e-mail']} - Usage quota exceeded, [contact support](mailto:developers@steamship.com) for more credits.")
+        else:
+            placeholder.markdown(
+                f"Signed in as {usage_stats['e-mail']} - Usage quota {usage_stats[APP_ID]}/{USAGE_LIMIT}")
+    else:
+        placeholder.markdown("Not logged in yet")
+
+
+def get_user_stats(email: str):
+    worksheet = get_worksheet()
+    labels = worksheet.row_values(1)
+
+    user_stats = worksheet.find(email, in_column=0, case_sensitive=False)
+    if not user_stats:
+        time.sleep(random.randint(0, 10))
+        user_stats = worksheet.find(email, in_column=0, case_sensitive=False)
+
+    if user_stats:
+        user_id = user_stats.row
+        usage_stats = worksheet.row_values(user_stats.row)
+        print(usage_stats)
+    else:
+        usage_stats = [email] + [0] * (len(labels) - 1)
+        worksheet.append_row(usage_stats)
+        user_id = worksheet.find(email, in_column=0, case_sensitive=False).row
+
+    usage_stats = {**{
+        label: int(value) if isinstance(value, int) or value.isdigit() else value for label, value in
+        zip(labels, usage_stats)
+    }, "id": user_id}
+
+    check_usage(usage_stats)
+    return usage_stats
+
+
+def update_usage():
+    usage_stats = st.session_state["usage_stats"]
+    print(f"Increasing usage for {usage_stats['e-mail']}")
+    worksheet = get_worksheet()
+    user_id = usage_stats["id"]
+    usage_stats[APP_ID] += 1
+    worksheet.update(f'A{user_id}:{user_id}', [list(usage_stats.values())[:-1]])
+
+    if usage_stats[APP_ID] > USAGE_LIMIT:
+        st.markdown("max usage contact support.")
+    check_usage(usage_stats)
+    st.session_state["usage_stats"] = usage_stats
+
+
+def get_google_oauth_client():
+    client_id = st.secrets["google_oauth_client_id"]
+    client_secret = st.secrets["google_oauth_client_secret"]
+    return GoogleOAuth2(client_id, client_secret)
+
+
+def check_auth() -> None:
+    client = get_google_oauth_client()
+
+    redirect_uri = st.secrets["redirect_uri"]
+    authorization_url = asyncio.run(
+        client.get_authorization_url(
+            redirect_uri, scope=["email", "profile"]
+        )
+    )
+
+    if "user_email" not in st.session_state:
+        try:
+            code = st.experimental_get_query_params()["code"]
+            token = asyncio.run(client.get_access_token(code, redirect_uri))
+
+            if token.is_expired():
+                show_login_prompt(authorization_url)
+            else:
+                _, user_email = asyncio.run(client.get_id_email(token["access_token"]))
+                st.session_state["user_email"] = user_email
+
+        except (KeyError, GetAccessTokenError):
+            show_login_prompt(authorization_url)
+
+    user_email = st.session_state["user_email"]
+    user_stats = get_user_stats(user_email)
+    st.session_state["usage_stats"] = user_stats
+
+
+def show_login_prompt(authorization_url):
+    st.markdown("Please sign in to use our app:")
+
+    image = Path("google_login_button.png")
+    file_ = image.open("rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+
+    st.write(
+        f"""<a target="_blank"
+                                  href="{authorization_url}">
+                                  <image src="data:image/gif;base64,{data_url}" width="200px">
+                                  </a>""",
+        unsafe_allow_html=True,
+    )
+    st.stop()
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,12 @@
 import re
 from datetime import datetime
 from itertools import groupby
+from pathlib import Path
 
 import streamlit as st
 
 from api_spec import Message, Intent, Sentiment
+from src.auth import check_auth, update_usage, check_usage
 from src.chat_utils import analyze_chat_stream
 from src.data import EXAMPLES
 from src.ui import display_intent_stats, display_sentiment_stats, display_threads, display_messages_by_intent, footer
@@ -24,8 +26,30 @@ INTERESTING_SENTIMENTS = {Sentiment.NEGATIVE: {"title": "Negative", "color": '#F
 def main():
     st.set_page_config(
         page_title='Chat Analytics',
-        page_icon="🚢"
+        page_icon=Path("data/logo.png").open("rb").read()
     )
+    html_string = """
+    <!-- Hotjar Tracking Code for https://steamship.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3131229,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+    """
+    st.markdown(html_string, unsafe_allow_html=True)
+
+    print(Path("data/logo.png").resolve())
+
+    placeholder = st.empty()
+    st.session_state["placeholder"] = placeholder
+    check_usage(st.session_state.get("usage_stats"))
+
+    check_auth()
 
     st.title("Chat Analytics")
     example = st.selectbox("Example", options=EXAMPLES)
@@ -48,6 +72,7 @@ def main():
             for i, (user, message) in enumerate(re.findall(r"(\w+):\s*(.*)", text))
         ]
         with st.spinner("Processing..."):
+            update_usage()
             processed_chat_stream = analyze_chat_stream(chat_stream)
 
             intent_to_messages = {k: list(v) for k, v in
