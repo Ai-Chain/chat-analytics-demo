a2b5093c8eda9bad07da3f0e8b78119a9ed3c5a1
Enias Cailliau <hello@enias.io>
2022-11-07 19:49:04 +0100

    Upgrade to 2.2

--- src/chat_utils.py
+++ src/chat_utils.py
@@ -3,15 +3,15 @@ from typing import List
 
 import streamlit as st
 from pydantic import parse_obj_as
-from steamship import AppInstance, Steamship
+from steamship import Steamship, PackageInstance
 
 from api_spec import Message
 
-APP_HANDLE = "chat-analytics-app-t"
+APP_HANDLE = "chat-analytics"
 
 
 @st.cache(ttl=3600, allow_output_mutation=True)
-def get_app_instance(api_key: str) -> AppInstance:
+def get_app_instance(api_key: str) -> PackageInstance:
     """Get an instance of the chat-analytics-app."""
     client = Steamship(
         api_key=api_key,
@@ -19,17 +19,17 @@ def get_app_instance(api_key: str) -> AppInstance:
         app_base="https://steamship.run/",
         web_base="https://app.steamship.com/",
     )
-    return AppInstance.create(client, app_handle=APP_HANDLE, handle=APP_HANDLE, upsert=True).data
+    return client.use(package_handle=APP_HANDLE, instance_handle=APP_HANDLE, fetch_if_exists=False)
 
 
 def analyze_chat_stream(chat_stream: List[Message]) -> List[Message]:
     """Test analyze endpoint."""
     app_instance = get_app_instance(api_key=st.secrets["steamship_api_key"])
 
-    response = app_instance.post(
+    response = app_instance.invoke(
         "analyze",
         chat_stream=[message.dict(format_dates=True, format_enums=True) for message in chat_stream],
     )
-    processed_chat_stream = parse_obj_as(List[Message], response.data["chat_stream"])
+    processed_chat_stream = parse_obj_as(List[Message], response["chat_stream"])
 
     return processed_chat_stream
